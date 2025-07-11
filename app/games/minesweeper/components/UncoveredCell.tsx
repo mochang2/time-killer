228e55e0004e41e9c4ab9dc70ec5824b30ddd426
changmolee <changmolee@netmable.com>
2025-07-11 16:47:26 +0900

    #3(feat): 셀 라렌더링 속도 개선

--- app/games/minesweeper/components/UncoveredCell.tsx
+++ app/games/minesweeper/components/UncoveredCell.tsx
@@ -1,16 +1,14 @@
-import Image from "next/image";
-
-import uncovered0 from "@/app/games/minesweeper/assets/uncovered-0.svg";
-import uncovered1 from "@/app/games/minesweeper/assets/uncovered-1.svg";
-import uncovered2 from "@/app/games/minesweeper/assets/uncovered-2.svg";
-import uncovered3 from "@/app/games/minesweeper/assets/uncovered-3.svg";
-import uncovered4 from "@/app/games/minesweeper/assets/uncovered-4.svg";
-import uncovered5 from "@/app/games/minesweeper/assets/uncovered-5.svg";
-import uncovered6 from "@/app/games/minesweeper/assets/uncovered-6.svg";
-import uncovered7 from "@/app/games/minesweeper/assets/uncovered-7.svg";
-import uncovered8 from "@/app/games/minesweeper/assets/uncovered-8.svg";
-import uncoveredExplodedMine from "@/app/games/minesweeper/assets/uncovered-exploded-mine.svg";
-import uncoveredMine from "@/app/games/minesweeper/assets/uncovered-mine.svg";
+import Uncovered0 from "@/app/games/minesweeper/assets/uncovered-0.svg";
+import Uncovered1 from "@/app/games/minesweeper/assets/uncovered-1.svg";
+import Uncovered2 from "@/app/games/minesweeper/assets/uncovered-2.svg";
+import Uncovered3 from "@/app/games/minesweeper/assets/uncovered-3.svg";
+import Uncovered4 from "@/app/games/minesweeper/assets/uncovered-4.svg";
+import Uncovered5 from "@/app/games/minesweeper/assets/uncovered-5.svg";
+import Uncovered6 from "@/app/games/minesweeper/assets/uncovered-6.svg";
+import Uncovered7 from "@/app/games/minesweeper/assets/uncovered-7.svg";
+import Uncovered8 from "@/app/games/minesweeper/assets/uncovered-8.svg";
+import UncoveredExplodedMine from "@/app/games/minesweeper/assets/uncovered-exploded-mine.svg";
+import UncoveredMine from "@/app/games/minesweeper/assets/uncovered-mine.svg";
 import type { Position } from "@/app/games/minesweeper/types";
 
 type Properties = Position & {
@@ -21,40 +19,40 @@ type Properties = Position & {
 
 export default function UncoveredCell({ y, x, isMine, isExploded, value }: Properties) {
   if (isMine && isExploded) {
-    return <span className="inline-block w-6 h-6 bg-orange-950 " data-cell={ `${y},${x}` }><Image alt="uncovered-exploded-mine" height={ 24 } src={ uncoveredExplodedMine.src } width={ 24 } /></span>;
+    return <span className="inline-block w-6 h-6 bg-orange-950 " data-cell={ `${y},${x}` }><UncoveredExplodedMine height={ 24 } width={ 24 } /></span>;
   }
 
   if (isMine) {
-    return <span className="inline-block w-6 h-6 bg-orange-950 " data-cell={ `${y},${x}` }><Image alt="uncovered-mine" height={ 24 } src={ uncoveredMine.src } width={ 24 } /></span>;
+    return <span className="inline-block w-6 h-6 bg-orange-950 " data-cell={ `${y},${x}` }><UncoveredMine height={ 24 } width={ 24 } /></span>;
   }
 
   switch (value) {
   case 1: {
-    return <span data-cell={ `${y},${x}` }><Image alt="uncovered-1" height={ 24 } src={ uncovered1.src } width={ 24 } /></span>;
+    return <span data-cell={ `${y},${x}` }><Uncovered1 height={ 24 } width={ 24 } /></span>;
   }
   case 2: {
-    return <span data-cell={ `${y},${x}` }><Image alt="uncovered-2" height={ 24 } src={ uncovered2.src } width={ 24 } /></span>;
+    return <span data-cell={ `${y},${x}` }><Uncovered2 height={ 24 } width={ 24 } /></span>;
   }
   case 3: {
-    return <span data-cell={ `${y},${x}` }><Image alt="uncovered-3" height={ 24 } src={ uncovered3.src } width={ 24 } /></span>;
+    return <span data-cell={ `${y},${x}` }><Uncovered3 height={ 24 } width={ 24 } /></span>;
   }
   case 4: {
-    return <span data-cell={ `${y},${x}` }><Image alt="uncovered-4" height={ 24 } src={ uncovered4.src } width={ 24 } /></span>;
+    return <span data-cell={ `${y},${x}` }><Uncovered4 height={ 24 } width={ 24 } /></span>;
   }
   case 5: {
-    return <span data-cell={ `${y},${x}` }><Image alt="uncovered-5" height={ 24 } src={ uncovered5.src } width={ 24 } /></span>;
+    return <span data-cell={ `${y},${x}` }><Uncovered5 height={ 24 } width={ 24 } /></span>;
   }
   case 6: {
-    return <span data-cell={ `${y},${x}` }><Image alt="uncovered-6" height={ 24 } src={ uncovered6.src } width={ 24 } /></span>;
+    return <span data-cell={ `${y},${x}` }><Uncovered6 height={ 24 } width={ 24 } /></span>;
   }
   case 7: {
-    return <span data-cell={ `${y},${x}` }><Image alt="uncovered-7" height={ 24 } src={ uncovered7.src } width={ 24 } /></span>;
+    return <span data-cell={ `${y},${x}` }><Uncovered7 height={ 24 } width={ 24 } /></span>;
   }
   case 8: {
-    return <span data-cell={ `${y},${x}` }><Image alt="uncovered-8" height={ 24 } src={ uncovered8.src } width={ 24 } /></span>;
+    return <span data-cell={ `${y},${x}` }><Uncovered8 height={ 24 } width={ 24 } /></span>;
   }
   default: {
-    return <span data-cell={ `${y},${x}` }><Image alt="uncovered-0" height={ 24 } src={ uncovered0.src } width={ 24 } /></span>;
+    return <span data-cell={ `${y},${x}` }><Uncovered0 height={ 24 } width={ 24 } /></span>;
   }
   }
 }
